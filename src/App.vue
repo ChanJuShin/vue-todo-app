43905bb134a37661f9cae5bd6a87030003d08ca0
ChanJuShin <ckswn1259@naver.com>
2021-04-14 17:04:37 +0900

    공통 CSS 적용 및 TodoHeader 구조추가

--- src/App.vue
+++ src/App.vue
@@ -8,10 +8,10 @@
 </template>
 
 <script>
-import TodoHeader from "@/components/TodoHeader";
-import TodoInput from "@/components/TodoInput";
-import TodoList from "@/components/TodoList";
-import TodoFooter from "@/components/TodoFooter";
+import TodoHeader from "./components/TodoHeader";
+import TodoInput from "./components/TodoInput";
+import TodoList from "./components/TodoList";
+import TodoFooter from "./components/TodoFooter";
 
 export default {
   name: 'App',
@@ -25,4 +25,18 @@ export default {
 </script>
 
 <style>
+body {
+  text-align: center;
+  background-color: #F6F6F6;
+}
+input {
+  border-style: groove;
+  width: 200px;
+}
+button {
+  border-style: groove;
+}
+.shadow {
+  box-shadow: 5px 10px 10px rgba(0, 0, 0, 0.03);
+}
 </style>
